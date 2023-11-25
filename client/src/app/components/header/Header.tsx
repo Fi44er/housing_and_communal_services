2a384d2cedd52b581d5e640b78c2ae824ef927b0
Valey <valej200554@gmail.com>
2023-11-25 11:10:18 +0500

    add

---
+++ client/src/app/components/header/Header.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import "./Header.css";
+import { useState } from "react";
+
+export default function Header() {
+  const [isOpen, setOpen] = useState(false);
+  const [isLogin, setLogin] = useState(false);
+  const [isReg, setReg] = useState(false);
+  return (
+    <header className="header">
+      <div className="headerLogo">
+        <Image
+          className="logo1"
+          src="/Group.svg"
+          width={61}
+          height={61}
+          alt="Logo"
+        />
+        <Image
+          className="logo2"
+          src="/Group.svg"
+          width={31}
+          height={31}
+          alt="Logo"
+        />
+        <h2>Уютный город</h2>
+      </div>
+      <nav className={`headerNav ${isOpen ? "active" : ""}`}>
+        <ul className="headerNavList">
+          <li className="headerNavItem">
+            <Link href="/">Главная </Link>
+          </li>
+          <li className="headerNavItem">
+            <Link href="/about_us">Контакты</Link>
+          </li>
+          <li className="headerNavItem">
+            <Link href="#">О ЖКХ </Link>
+          </li>
+          <li className="headerNavItem">
+            <Link href="#">Раписание</Link>
+          </li>
+          <li className="headerNavItem">
+            <Link href="/news">Новости</Link>
+          </li>
+          <div className="headerButtonMobile">
+            <div>
+              <button>Войти</button>
+            </div>
+            <div>
+              <button>Регистрация</button>
+            </div>
+          </div>
+        </ul>
+      </nav>
+      <div className="headerButton">
+        <div>
+          <button onClick={() => setLogin(!isLogin)}>Войти</button>
+        </div>
+        <div>
+          <button onClick={() => setReg(!isReg)}>Регистрация</button>
+        </div>
+      </div>
+      <div className="headerMenu">
+        <button onClick={() => setOpen(!isOpen)}>
+          <Image src="/Category.svg" width={40} height={40} alt="" />
+        </button>
+      </div>
+    
+    </header>
+  );
+}
